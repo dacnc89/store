7652af9591735983a9dbb85926f65743b5918d0b
nguyendac8 <congdacit@gmail.com>
2017-09-18 20:43:35 -0400

    display products

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@ Rails.application.routes.draw do
   #resources :products
 #  get '/:brand_name/:amount', to: 'pages#brand'
   get '/:category_name/:amount', to: 'pages#category'
-  get '/:product_name', to: 'products#show'
-
-
+  get '/:product_name', to: 'products#show', as: 'product'
+  
+  #create order item when click add to cart button
+  post '/order_items', to: 'order_items#add_order_item'
+  
   root to: 'products#index'
   get 'pages/index'
-
+  get 'checkout/cart/details', to: 'carts#show', as: 'cart'
+  get 'checkout/c/c/c', to: 'orders#new', as: 'check_out'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
