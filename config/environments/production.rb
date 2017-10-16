372158219d19255ed71e8c06847b012121102ab5
nguyendac8 <congdacit@gmail.com>
2017-10-16 03:34:29 -0400

    turn on config.assets.compress in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,9 +26,13 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  
+  # Add for error javascript not working after deploy to heroku
+  config.assets.compress = true
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
